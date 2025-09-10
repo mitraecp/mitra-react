8219e9fcdea29d3930eb76b90dc96d0959d9858f
Joao Quirino <joaoluistq@gmail.com>
2025-09-10 15:52:24 -0300

    params log

--- src/components/DynamicRenderer.tsx
+++ src/components/DynamicRenderer.tsx
@@ -638,7 +638,7 @@ const componentRegistry = {
   },
   updateMitraComponents: async (params: any, componentId?: string | null): Promise<any> => {
     try {
-      console.log(`updateMitraComponents(${params ? JSON.stringify(params) : ''})`);
+      console.log(`updateMitraComponents:`, params);
 
       const result = await messageService.sendInteraction('updateComponents', { params }, componentId);
       console.log(`updateMitraComponents result:`, result);
