68fb3aede84d99315f2ac22039fc5b3c8be0c000
Joao Quirino <joaoluistq@gmail.com>
2025-09-10 14:56:49 -0300

    updateMitraComponents

--- src/lib/message-service.ts
+++ src/lib/message-service.ts
@@ -139,7 +139,7 @@ export class MessageService {
     }
 
     // Lista de tipos de interações que são assíncronas
-    const asyncInteractions = ['query', 'action', 'form', 'dbaction', 'variable', 'goToScreen', 'variableQuery'];
+    const asyncInteractions = ['query', 'action', 'form', 'dbaction', 'variable', 'goToScreen', 'variableQuery', 'updateComponents'];
 
     // Se for uma interação assíncrona, retornar uma Promise
     if (asyncInteractions.includes(interactionType)) {
